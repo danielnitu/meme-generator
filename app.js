3d076a5085086503cf4287575ff3793c7663ed2b
Daniel Nitu <nitu.contact@yahoo.com>
2016-09-13 13:39:50 +0300

    Main Files
    Main app files

---
+++ app.js
@@ -0,0 +1,42 @@
+// URL validation function
+function validUrl(url) {
+    return /^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(url);
+}
+
+var main = function() {
+  var $topText;
+	$('#top-text').keyup(function() {
+    $topText = $(this).val();
+    $('.top-caption').text($topText);
+  });
+  
+  var $bottomText;
+  $('#bottom-text').keyup(function() {
+    $bottomText = $(this).val();
+    $('.bottom-caption').text($bottomText);
+  });
+  
+  var $imageUrl;
+  $('#image-url').keyup(function() {
+    $imageUrl = $(this).val();
+    if (validUrl($imageUrl)) {
+      $('.meme img').attr('src', $imageUrl);
+    } else {
+      $(this).val('Enter a valid URL');
+    }
+  });
+  
+  $('#image-url, #top-text, #bottom-text').on('focus', function() {
+    $(this).val('');
+  }).on('blur', function() {
+    if ($(this).is('#top-text')) {
+      $(this).val($topText);
+    } else if ($(this).is('#bottom-text')) {
+      $(this).val($bottomText);
+    } else if ($(this).is('#image-url')) {
+      $(this).val($imageUrl);
+    }
+  });
+};
+
+$(document).ready(main);
